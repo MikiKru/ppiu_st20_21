907c723e0f89e690e4b6a91f2c8c53fc70290cd0
MikiKru <michal_kruczkowski@o2.pl>
2021-04-28 09:43:58 +0200

    ppiu1 v0

---
+++ src/main/java/pl/utp/ppiu/PpiuApplication.java
@@ -0,0 +1,13 @@
+package pl.utp.ppiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpiuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/utp/ppiu/PpiuApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.utp.ppiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpiuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
